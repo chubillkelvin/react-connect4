6605dc074a4106959567cddac824ab3293bed5a6
Kelvin Chu <chubillkelvin@gmail.com>
2019-04-16 15:43:55 +0800

    Update DrawRow component.

--- src/module/game/component/DragRow/index.tsx
+++ src/module/game/component/DragRow/index.tsx
@@ -8,13 +8,35 @@ interface Props {
     currentPlayer: Player;
 }
 
-class DragRow extends React.PureComponent<Props> {
+interface State {
+    activeDrags: number;
+}
+
+class DragRow extends React.PureComponent<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            activeDrags: 0,
+        };
+    }
+
+    onStart = () => {
+        this.setState({activeDrags: this.state.activeDrags + 1});
+    };
+
+    onStop = () => {
+        this.setState({activeDrags: this.state.activeDrags - 1});
+    };
+
     render() {
         const {currentPlayer} = this.props;
+        const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
         return (
             <div className="dragrow">
-                <Draggable grid={[100, 100]}>
-                    <Piece currentPlayer={currentPlayer} />
+                <Draggable {...dragHandlers} grid={[100, 100]} bounds="parent">
+                    <div className="draggableDiv">
+                        <Piece currentPlayer={currentPlayer} />
+                    </div>
                 </Draggable>
             </div>
         );
