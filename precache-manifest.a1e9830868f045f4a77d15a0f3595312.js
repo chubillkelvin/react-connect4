59b9e2707c1e2921cf11bacd3af85499fbd86ba1
Kelvin Chu <chubillkelvin@gmail.com>
2019-04-18 10:15:49 +0800

    Updates

---
+++ precache-manifest.a1e9830868f045f4a77d15a0f3595312.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a76ad0f26911eb2b37b3",
+    "url": "/react-connect4/static/css/main.8097888c.chunk.css"
+  },
+  {
+    "revision": "a76ad0f26911eb2b37b3",
+    "url": "/react-connect4/static/js/main.4a002d2b.chunk.js"
+  },
+  {
+    "revision": "14f307fc71fdb7c76087",
+    "url": "/react-connect4/static/js/runtime~main.966f089c.js"
+  },
+  {
+    "revision": "f7c1bf42fde941c50085",
+    "url": "/react-connect4/static/js/2.ba395629.chunk.js"
+  },
+  {
+    "revision": "7182f328c25f4b6d29430f1cc896a3c7",
+    "url": "/react-connect4/static/media/tick.7182f328.wav"
+  },
+  {
+    "revision": "1451830df9b2d18db8a2951becb0c6ac",
+    "url": "/react-connect4/static/media/click.1451830d.png"
+  },
+  {
+    "revision": "0ed7f90775fdee2277ddc20353ccb351",
+    "url": "/react-connect4/static/media/win.0ed7f907.wav"
+  },
+  {
+    "revision": "b1e7ece6b45ee7c339bae53062722e1d",
+    "url": "/react-connect4/index.html"
+  }
+];
